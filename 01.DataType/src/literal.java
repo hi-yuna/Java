5e46c0f58cd27dc86bf3568cd0b167828e27d1f1
yuna <ouna0223@naver.com>
2021-08-11 22:18:12 +0900

    literal 출력하기

---
+++ 01.DataType/src/literal.java
@@ -0,0 +1,20 @@
+
+public class literal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(123);
+		System.out.println(3.14F);
+		System.out.println(3.14);
+		System.out.println('A');
+		System.out.println('나');
+		System.out.println('\uC720');
+		System.out.println('\uB098');
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println('\t'+"홍길동");
+		
+	}
+
+}
