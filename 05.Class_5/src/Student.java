1b3a637c16421e16f8d34b780e716ec74d2f18c6
yuna <ouna0223@naver.com>
2021-08-20 23:26:35 +0900

    Class_생성자

---
+++ 05.Class_5/src/Student.java
@@ -0,0 +1,20 @@
+
+public class Student {
+
+	String name;
+	int age;
+	String address;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, int age, String address) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+
+}
---
+++ 05.Class_5/src/TestStudent.java
@@ -0,0 +1,14 @@
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student stu = new Student("나유나", 25, "광주");
+		System.out.println(stu.name);
+		System.out.println(stu.age);
+		System.out.println(stu.address);
+		
+	}
+
+}
